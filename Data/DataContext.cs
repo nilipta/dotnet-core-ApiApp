4940cdd1cabc6d3dc041aadb65e70bb699971ee9
nilipta <niliptasatapathy@gmail.com>
2023-07-02 14:38:26 +0530

    added data & entity

---
+++ Data/DataContext.cs
@@ -0,0 +1,13 @@
+using ApiApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<AppUser> Users { get; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
+using ApiApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(optionsAction =>
+{
+    optionsAction.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
