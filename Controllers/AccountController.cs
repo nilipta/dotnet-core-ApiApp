5e1d64c2e72b715596b16166130fb229b9d2904c
nilipta <niliptasatapathy@gmail.com>
2023-07-02 21:09:34 +0530

    added Register & login DTO

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -37,6 +37,23 @@ namespace ApiApp.Controllers
             return user;
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName.ToLower() == loginDto.UserName.ToLower());
+            if (user == null) return Unauthorized("Invalid username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password")
+            }
+
+            return user;
+        }
+
         private async Task<bool> UserExist(string Username)
         {
             return await _context.Users.AnyAsync(x => x.UserName == Username.ToLower());
--- DTOs/RegisterDto.cs
+++ DTOs/RegisterDto.cs
@@ -1,8 +1,15 @@
+
+
+using System.ComponentModel.DataAnnotations;
+
 namespace ApiApp.DTOs
 {
     public class RegisterDto
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
         public string Password { get; set; }
     }
 
